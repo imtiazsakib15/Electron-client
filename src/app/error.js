76088f13a17cb1b4d9a500a6a542e6bd5709f58c
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-02-12 19:36:37 +0600

    Error page added

--- src/app/error.js
+++ src/app/error.js
@@ -1,14 +1,16 @@
+"use client";
+
 import Button from "@/components/Button/Button";
 import Link from "next/link";
 
-const NotFound = () => {
+const error = () => {
   return (
     <main className="h-screen w-full flex flex-col justify-center items-center">
       <h1 className="text-7xl sm:text-8xl lg:text-9xl font-extrabold tracking-widest">
-        404
+        Oops
       </h1>
       <div className="bg-green-600 text-white px-2 text-xs sm:text-sm rounded rotate-12 absolute -mt-10 sm:-mt-4">
-        Page Not Found
+        An Error Occured
       </div>
       <Link href={"/"} className="mt-8">
         <Button>Go Home</Button>
@@ -17,4 +19,4 @@ const NotFound = () => {
   );
 };
 
-export default NotFound;
+export default error;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+const Button = ({ children }) => {
+  return (
+    <span className="relative inline-block text-sm font-medium text-green-600 group active:text-green-600 focus:outline-none focus:ring">
+      <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-green-600 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+      <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+        {children}
+      </span>
+    </span>
+  );
+};
+
+export default Button;
