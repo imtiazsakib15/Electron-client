0f32197349390c88133011311c880021f7ae0d32
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-03-16 00:42:26 +0600

    Table ui added in all category page

--- src/app/(dashboard)/dashboard/categories/all/page.js
+++ src/app/(dashboard)/dashboard/categories/all/page.js
@@ -1,10 +1,20 @@
-import { getAllCategory } from "@/actions/categoryActions";
+export const metadata = {
+  title: "All Category | Electron",
+  description: "...",
+};
 
-const AllCategory = async () => {
+import { getAllCategory } from "@/actions/categoryActions";
+import AllCategory from "@/components/AllCategory/AllCategory";
+
+const AllCategoryPage = async () => {
   const categories = await getAllCategory();
   console.log(categories);
 
-  return <div></div>;
+  return (
+    <div className="mx-auto py-8 sm:py-10 lg:py-14">
+      <AllCategory categories={categories} />
+    </div>
+  );
 };
 
-export default AllCategory;
+export default AllCategoryPage;
---
+++ src/components/AllCategory/AllCategory.js
@@ -0,0 +1,77 @@
+"use client";
+
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+
+const AllCategory = ({ categories }) => {
+  const rows = [
+    {
+      key: "1",
+      name: "Tony Reichert",
+      role: "CEO",
+      status: "Active",
+    },
+    {
+      key: "2",
+      name: "Zoey Lang",
+      role: "Technical Lead",
+      status: "Paused",
+    },
+    {
+      key: "3",
+      name: "Jane Fisher",
+      role: "Senior Developer",
+      status: "Active",
+    },
+    {
+      key: "4",
+      name: "William Howard",
+      role: "Community Manager",
+      status: "Vacation",
+    },
+  ];
+
+  const columns = [
+    {
+      key: "name",
+      label: "NAME",
+    },
+    {
+      key: "role",
+      label: "ROLE",
+    },
+    {
+      key: "status",
+      label: "STATUS",
+    },
+  ];
+  return (
+    <div>
+      <Table aria-label="Example table with dynamic content">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={rows}>
+          {(item) => (
+            <TableRow key={item.key}>
+              {(columnKey) => (
+                <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default AllCategory;
